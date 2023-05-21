4b7db299edcf9a1519a7d9cf76d31ee2cd549c9a
Gabriel <gabriel59kg@gmail.com>
2023-05-20 22:32:56 -0400

    methods

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     simple();
     structs1();
     structs2();
+    structs3();
 }
 
 fn simple() {
@@ -53,4 +54,28 @@ fn structs2() {
 
     println!("2: Debug `rect` is -> \n {:?}", rect);
     println!("2: PrettyPrint `rect` is -> \n {:#?}", rect);
+}
+
+fn structs3() {
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    impl Rectangle {
+        // self/Self is a magic word (not just convention)
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+    }
+
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "3: The area of the rectangle is {} square pixels.",
+        rect.area()
+    );
 }
