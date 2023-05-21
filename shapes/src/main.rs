0e19fd4a8afc339210a6abd652d1b8e3fb435be9
Gabriel <gabriel59kg@gmail.com>
2023-05-20 22:24:46 -0400

    derive traits

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     simple();
-    with_structs();
+    structs1();
+    structs2();
 }
 
 fn simple() {
@@ -8,7 +9,7 @@ fn simple() {
     let height = 50;
 
     println!(
-        "1: The area of the rectangle is {} square pixels.",
+        "0: The area of the rectangle is {} square pixels.",
         area(width, height)
     );
 
@@ -17,23 +18,39 @@ fn simple() {
     }
 }
 
-fn with_structs() {
-    let rect1 = Rectangle {
+fn structs1() {
+    let rect = Rectangle {
         width: 30,
         height: 50,
     };
-
-    print!(
-        "2: The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-
-    fn area(rectangle: &Rectangle) -> u32 {
-        rectangle.width * rectangle.height
-    }
 
     struct Rectangle {
         width: u32,
         height: u32,
     }
+
+    println!(
+        "1: The area of the rectangle is {} square pixels.",
+        area(&rect)
+    );
+
+    fn area(rectangle: &Rectangle) -> u32 {
+        rectangle.width * rectangle.height
+    }
+}
+
+fn structs2() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+
+    println!("2: Debug `rect` is -> \n {:?}", rect);
+    println!("2: PrettyPrint `rect` is -> \n {:#?}", rect);
 }
